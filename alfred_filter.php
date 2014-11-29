52ea43019fca2a530657e029039cf0ea01b63976
xeaon <kemal@soyguder.de>
2014-11-29 21:29:58 +0100

    Update alfred_filter.php

--- alfred_filter.php
+++ alfred_filter.php
@@ -3,10 +3,10 @@
  * alfred_filter.php
  * by Kemal Soyguder <kemal@freifunk-troisdorf.de>
 
- * This work is licensed under the Creative Commons //
- * Attribution-NonCommercial-ShareAlike 4.0 International License. //
- * To view a copy of this license, visit //
- * http://creativecommons.org/licenses/by-nc-sa/4.0/. 
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
 */
 
 /*
